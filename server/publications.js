ffe972071e648ed07f732069a402f55ce04cfd38
Daniel Rodriguez <daniel.rodriguez.ramirez.contractor@bbva.com>
2015-04-27 10:17:16 +0200

    Routing

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,11 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+
+/*
+// we get Tom Coleman post without url
+Meteor.publish('posts', function() {
+  return Posts.find({'author':'Tom Coleman'},
+  	{fields: {url: false}});
+});
+*/
