28d0606b27d36a55848f8ee3e6fe255c7372f61d
Daniel Rodriguez <daniel.rodriguez.ramirez.contractor@bbva.com>
2015-04-27 09:16:50 +0200

    Remove autopublish

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,17 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducción a Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor (framework utilizado)',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'Libro de Meteor',
-    url: 'http://themeteorbook.com'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
