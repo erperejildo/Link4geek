135f88b92aa18ef23b2298ee617372d28cae2f10
Daniel Rodriguez <daniel.rodriguez.ramirez.contractor@bbva.com>
2015-04-29 16:46:52 +0200

    Force url unique

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -12,6 +12,10 @@ Template.postSubmit.events({
 			if (error) {
 				return alert(error.reason);
 			}
+			// show this result but route anyway
+	      	if (result.postExists) {
+		        alert('Esta URL ya existe');
+		    }
 			Router.go('postPage', {
 				_id: result._id
 			});
