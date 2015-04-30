8a53c69ffef25a6fd814e4b5d2cdd751e39b4174
Daniel Rodriguez <daniel.rodriguez.ramirez.contractor@bbva.com>
2015-04-30 14:03:42 +0200

    Add posts and comments

--- server/fixtures.js
+++ server/fixtures.js
@@ -3,11 +3,15 @@ if (Posts.find().count() === 0) {
 
   // create two users
   var tomId = Meteor.users.insert({
-    profile: { name: 'Tom Coleman' }
+    profile: {
+      name: 'Tom Coleman'
+    }
   });
   var tom = Meteor.users.findOne(tomId);
   var sachaId = Meteor.users.insert({
-    profile: { name: 'Sacha Greif' }
+    profile: {
+      name: 'Sacha Greif'
+    }
   });
   var sacha = Meteor.users.findOne(sachaId);
 
@@ -53,4 +57,24 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0
   });
+
+  Posts.insert({
+    title: 'The Meteor Book',
+    userId: tom._id,
+    author: tom.profile.name,
+    url: 'http://themeteorbook.com',
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0
+  });
+
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000),
+      commentsCount: 0
+    });
+  }
 }
