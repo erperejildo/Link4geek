c430dad5d026c36e9f647d1d9fa501918280770b
Daniel Rodriguez <daniel.rodriguez.ramirez.contractor@bbva.com>
2015-04-30 14:48:05 +0200

    Single post subscription

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,14 @@
 Meteor.publish('posts', function(options) {
-  check(options, {
-    sort: Object,
-    limit: Number
-  });
-  return Posts.find({}, options);
+	check(options, {
+		sort: Object,
+		limit: Number
+	});
+	return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function(id) {
+	check(id, String)
+	return Posts.find(id);
 });
 
 /*
